52d811f0b96d216a1d41ef16fe82ef2b3d0c4c0f
Rauulll <nyaokeraul003@gmail.com>
2025-01-09 09:59:21 +0300

    -Add Makefile to run /scripts. -Update script files.

---
+++ code/enceladus_db/build_1.sql
@@ -0,0 +1,14 @@
+drop table if exists master_plan;
+create table master_plan(
+    start_time_utc text,
+    duration text,
+    date text,
+    team text,
+    spass_type text,
+    target text,
+    request_name text,
+    library_definition text,
+    title text,
+    description text
+);
+COPY import.master_plan FROM '/var/lib/postgresql/cassini_data/curious_data/data/master_plan.csv' WITH DELIMITER ',' HEADER CSV;
