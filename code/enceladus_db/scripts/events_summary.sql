e1bbb6ea01adba85f634ba1c13cf88c09257b6b4
Rauulll <nyaokeraul003@gmail.com>
2025-01-20 11:22:47 +0300

    create events_summary view

--- code/enceladus_db/scripts/events_summary.sql
+++ code/enceladus_db/scripts/events_summary.sql
@@ -1,6 +1,9 @@
 select time_stamp,
+       time_stamp::date as date,
        public.targets.description as targets,
+       public.event_types.description as event,
        title
 from events
-         inner join targets on target_id=targets.id
-where title ~* '^T\d.*? flyby';
+left join event_types on event_type_id=event_types.id
+left join targets on target_id=targets.id
+where title ilike '%flyby%' or title ilike '%fly by%';
