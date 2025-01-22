541e71d07190d6c389c128df61ee12485f639ebf
Rauulll <nyaokeraul003@gmail.com>
2025-01-22 18:37:35 +0300

    created a full-text search.

---
+++ code/enceladus_db/scripts/enceladus_events_view.sql
@@ -0,0 +1,15 @@
+drop view if exists enceladus_events;
+create view enceladus_events as
+select
+    events.id,
+    events.time_stamp::date as date,
+    events.title,
+    events.description,
+    event_types.description as event,
+    to_tsvector(
+    concat(events.description, '', events.title)
+    ) as search
+from events
+         inner join event_types on event_types.id = events.event_type_id
+where target_id = '46'
+order by events.time_stamp;
