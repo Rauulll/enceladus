a2bfbf8240a43f909ec7a901c3cd19ae056dfb84
Rauulll <nyaokeraul003@gmail.com>
2025-02-20 19:51:25 +0300

    -Clean up get_closest_flybys.sql to use get_closest_flybys.sql function.

--- code/enceladus_db/scripts/flyby_altitudes.sql
+++ code/enceladus_db/scripts/flyby_altitudes.sql
@@ -1,7 +1,9 @@
 drop materialized view if exists flyby_altitudes;
 create materialized view flyby_altitudes as
     select
-        (sclk::timestamp) as time_stamp,
+        sclk::timestamp as time_stamp,
+        date_part('year', sclk::timestamp) as year,
+        date_part('week', sclk::timestamp) as week,
         alt_t::numeric(10,3) as altitude
     from import_inms
     where target='ENCELADUS'
